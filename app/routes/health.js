0121ff67fbb3f62c211fec2c013bab3b30f045b3
Pedro Sotero Almeida <palmeida.terceiro@brasilcap.com.br>
2018-06-29 11:35:40 -0300

    getProjectName function

--- app/routes/health.js
+++ app/routes/health.js
@@ -1,10 +1,13 @@
+const getProjectName = function () {
+    const pwd = process.env.PWD;
+    const array = pwd.split('/');
+    const projectName = array[array.length - 1];
+    return projectName;
+}
 module.exports = (app) => {
     /** Health check route */
     app.get('/health', (req, res) => {
-        const pwd = process.env.PWD;
-        const array = pwd.split('/');
-        const projectName = array[array.length - 1];
         res.status(200);
-        return res.send(`The project ${projectName} is running fine.`);
+        return res.send(`The project ${getProjectName()} is running fine.`);
     });
 };
