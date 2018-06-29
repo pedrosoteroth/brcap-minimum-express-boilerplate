cbead4a619584dacd65aaab6c735686a905ef396
Pedro Sotero Almeida <palmeida.terceiro@brasilcap.com.br>
2018-06-29 11:03:14 -0300

    howto commonjs modules.

--- app/controller/orderController.js
+++ app/controller/orderController.js
@@ -1,13 +1,26 @@
+/**
+ * Module for orders
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} callback 
+ */
+
+/** GET order. */
 const getOrder = (req, res, callback) => {
     res.status(200);
-    res.send(`Hello current order ${req.query.type}`);
+    res.send(`Hello yout current order type is ${req.query.type}`);
 }
 
+/** POST order */
 const postOrder = (req, res, callback) => {
     res.status(200);
     res.send(`Hi ${req.body.name } you order ${req.body.type}`);
 }
 
+/**
+ * @exports getOrder 
+ * @exports postOrder
+ */
 module.exports = {
     getOrder,
     postOrder
--- app/routes/orders.js
+++ app/routes/orders.js
@@ -8,16 +8,33 @@ const {
 module.exports = (app) => {
     const orders = ['food', 'drinks', 'entertainment'];
 
+    /**     
+     *  JOI Scheme for query request.  
+     */
     const querySchema = Joi.object({
         type: Joi.string().required().valid(orders)
     });
 
+    /**     
+     *  JOI Scheme for body request.  
+     */
     const bodySchema = Joi.object({
         name: Joi.string().required(),
         type: Joi.string().required().valid(orders)
     })
 
+    /**     
+     *  GET orders.    
+     *  @param {string} type - The type of order valid [ food, drinks, entertainment ].     
+     *  @return {string} The current order type.     
+     */
     app.get('/orders', validator.query(querySchema), getOrder);
 
+    /**     
+     *  POST orders.    
+     *  @param {string} type - The type of order valid [ food, drinks, entertainment ].  
+     *  @param {string} name - The name of customer.   
+     *  @return {string} The current order type and customer name.     
+     */
     app.post('/orders', validator.body(bodySchema), postOrder);
 };
